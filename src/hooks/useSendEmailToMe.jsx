cab9972bb0672c9b775149e5d616bf1435aa5ef7
Gallo Garcia Nicolas <gallogarcianicolas@gmail.com>
2024-11-12 11:10:59 -0300

    :

--- src/hooks/useSendEmailToMe.jsx
+++ src/hooks/useSendEmailToMe.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+import emailjs from '@emailjs/browser'
+import React, { useRef, useState } from 'react'
 import {
   ButtonEmailToMe,
   Form,
@@ -10,54 +10,54 @@ import {
   Legend,
   TextArea,
   WrapperInputs,
-  WrapperTextArea,
-} from "../Components/Contacto/styles/contacto";
+  WrapperTextArea
+} from '../Components/Contacto/styles/contacto'
 
 const useSendEmailToMe = (showEmailToMe, setShowEmailToMe) => {
   const emailToMe = () => {
-    const form = useRef();
+    const form = useRef()
     const [datosEmail, setDatosEmail] = useState({
-      user_name: "",
-      user_email: "",
-      message: "",
-    });
+      user_name: '',
+      user_email: '',
+      message: ''
+    })
 
     const sendEmail = (e) => {
-      e.preventDefault();
+      e.preventDefault()
       //validar
 
       //enviar email
       emailjs
         .sendForm(
-          "service_atqh2ps",
-          "template_w2xdl7f",
+          'service_atqh2ps',
+          'template_w2xdl7f',
           form.current,
-          "ZcXlnk61KgGIwbFQS"
+          'ZcXlnk61KgGIwbFQS'
         )
         .then(
           (result) => {
-            console.log(result.text);
+            console.log(result.text)
           },
           (error) => {
-            console.log(error.text);
+            console.log(error.text)
           }
-        );
+        )
 
       //Resetear campos
       setDatosEmail({
-        user_name: "",
-        user_email: "",
-        message: "",
-      });
-    };
+        user_name: '',
+        user_email: '',
+        message: ''
+      })
+    }
     const onChange = (e) => {
-      e.preventDefault();
+      e.preventDefault()
       setDatosEmail({
         ...datosEmail,
-        [e.target.name]: e.target.value,
-      });
-    };
-    const { user_name, user_email, message } = datosEmail;
+        [e.target.name]: e.target.value
+      })
+    }
+    const { user_name, user_email, message } = datosEmail
     return (
       <Form ref={form} onSubmit={sendEmail}>
         <Legend>Contactame</Legend>
@@ -69,8 +69,7 @@ const useSendEmailToMe = (showEmailToMe, setShowEmailToMe) => {
               type="text"
               name="user_name"
               value={user_name}
-              onChange={onChange}
-            ></InputsContacto>
+              onChange={onChange}></InputsContacto>
           </WrapperInputs>
           <WrapperInputs>
             <Label>Tu email</Label>
@@ -79,19 +78,17 @@ const useSendEmailToMe = (showEmailToMe, setShowEmailToMe) => {
               type="email"
               name="user_email"
               onChange={(e) => onChange(e)}
-              value={user_email}
-            ></InputsContacto>
+              value={user_email}></InputsContacto>
           </WrapperInputs>
           <WrapperTextArea>
             <Label>Mensaje</Label>
             <TextArea
-              rows={"5"}
+              rows={'5'}
               name="message"
               placeholder="Escriba su mensaje aqui."
               onChange={onChange}
               value={message}
-              required
-            ></TextArea>
+              required></TextArea>
           </WrapperTextArea>
           <WrapperInputs>
             <InputSubmit type="submit" value="Enviar"></InputSubmit>
@@ -101,9 +98,9 @@ const useSendEmailToMe = (showEmailToMe, setShowEmailToMe) => {
           Volver a contactos
         </ButtonEmailToMe>
       </Form>
-    );
-  };
-  return [emailToMe];
-};
+    )
+  }
+  return [emailToMe]
+}
 
-export default useSendEmailToMe;
+export default useSendEmailToMe
